ead0041be47529e67c95493549e222eefba8a1f6
NikitaQ228 <121760391+NikitaQ228@users.noreply.github.com>
2024-03-30 20:56:22 +0400

    lab 4

---
+++ Server/Server/Server.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    HANDLE hPipe;
+    wchar_t buffer;
+    DWORD dwRead;
+
+    hPipe = CreateNamedPipe(TEXT("\\\\.\\pipe\\EchoNamedPipe"), PIPE_ACCESS_DUPLEX,
+        PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT,
+        1, 1, 1, NMPWAIT_USE_DEFAULT_WAIT, NULL);
+
+    if (hPipe == INVALID_HANDLE_VALUE) {
+        cerr << "Не удалось создать именованный канал. Код ошибки: " << GetLastError() << endl;
+        return 1;
+    }
+
+    cout << "Ожидание соединения с клиентом..." << endl;
+
+    ConnectNamedPipe(hPipe, NULL);
+
+    cout << "Клиент подключен! Ожидание данных..." << endl;
+
+    while (true) {
+        if (!ReadFile(hPipe, &buffer, sizeof(buffer), &dwRead, NULL) || dwRead == 0) {
+            cerr << "Потеря связи" << endl;
+            break;
+        }
+
+        if (buffer == L'\b') cout << "\b \b"; 
+        else if (buffer == '\r') cout << endl;
+        else wcout << buffer;
+
+        cout.flush();
+    }
+
+    CloseHandle(hPipe);
+
+    return 0;
+}
---
+++ Сlient/Сlient/Сlient.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    HANDLE hPipe;
+    char buffer;
+    DWORD dwWritten;
+
+    hPipe = CreateFile(TEXT("\\\\.\\pipe\\EchoNamedPipe"),
+                       GENERIC_READ | GENERIC_WRITE,
+                       0, NULL,
+                       OPEN_EXISTING,
+                       0, NULL);
+
+    if (hPipe == INVALID_HANDLE_VALUE) {
+        cerr << "Не удалось создать именованный канал. Код ошибки: " << GetLastError() << endl;
+        return 1;
+    }
+
+    cout << "Сервер подключен! Введите данные..." << endl;
+
+    while (true) {
+        
+        wchar_t c = _getwch();
+        if (c == L'\r') cout << '\n';
+        else if (c == L'\b') cout << "\b \b";
+        else wcout << c;
+
+        WriteFile(hPipe, &c, sizeof(c), &dwWritten, NULL);
+    }
+
+    CloseHandle(hPipe);
+    return 0;
+}
